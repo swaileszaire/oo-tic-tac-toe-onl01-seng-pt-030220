fc858c3c5eac1d90e4aac86c5e605702f4a6015b
Zaire  Swailes <swaileszaire@gmail.com>
2020-04-02 08:35:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,25 @@ class TicTacToe
   
   def play 
     
+  def initialize
+    @board = []
+  end 
+  
+  WIN_COMBINATIONS = [[0,4,8], [2,4,6], [3,4,5], [1,4,7], [0,3,6], [2,5,]] 
     
+  def display_board
+    @board = [[0,1,2][3,4,5][,6,7,8]]
+  end
+  
+  def input_to_index 
+    user_imput = [0,1,2,3,4,5,6,7,8].each do |x|
+      @board[|x|] 
+    end
+  end 
+    
+    def move(|x|, X)
+      
+  end 
+
+  
+end
